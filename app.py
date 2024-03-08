8b1cc7358f0027022b7fb21259c8b3f3ab3e3c85
Zhang Xiaoci <142374793+xxici@users.noreply.github.com>
2024-03-08 19:41:04 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+
+def index():
+    if request.method == "POST":
+        rate = float(request.form.get("rate"))
+        print(rate)
+        return(render_template("index.html"))
+    else:
+        return(render_template("index.html", result="Waiting for Exchange Rate......"))
+
+if __name__ == "__main__":
+    app.run()
