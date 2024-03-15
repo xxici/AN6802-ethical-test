60e49cdb385e67f174c8fb9c1c145dd6345ec40b
Zhang Xiaoci <142374793+xxici@users.noreply.github.com>
2024-03-15 19:05:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,31 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
-
 def index():
-    if request.method == "POST":
-        rate = float(request.form.get("rate"))
-        print(rate)
-        return(render_template("index.html"))
-    else:
-        return(render_template("index.html", result="Waiting for Exchange Rate......"))
+    return(render_template("index.html"))
+
+@app.route("/main", methods=['GET','POST'])
+def main():
+    name = request.form.get("name")
+    return(render_template("main.html", name=name))
+
+@app.route("/ethical_test", methods=['GET','POST'])
+def ethical_test():
+    return(render_template("ethical_test.html"))
+
+@app.route("/answer", methods=['GET','POST'])
+def answer():
+    ans = request.form["options"]
+    print(ans)
+    if ans == "true":
+        return(render_template("wrong.html"))
+    elif ans == "false":
+        return(render_template("correct.html"))
+
+
+@app.route("/end", methods=['GET','POST'])
+def end():
+    return(render_template("end.html"))
 
 if __name__ == "__main__":
     app.run()
