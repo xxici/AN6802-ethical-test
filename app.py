d39f76c77200a9d53a65c584e2ddfde2f693dee1
Zhang Xiaoci <142374793+xxici@users.noreply.github.com>
2024-03-15 20:39:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask,request,render_template
+import sqlite3
+import datetime
+from markupsafe import Markup
 
 app = Flask(__name__)
+
+name_flag = 0
+name = ""
 
 @app.route("/", methods=['GET','POST'])
 def index():
@@ -8,12 +14,36 @@ def index():
 
 @app.route("/main", methods=['GET','POST'])
 def main():
-    name = request.form.get("name")
+    global name_flag,name
+    if name_flag == 0:
+        name = request.form.get("name")
+        name_flag = 1
+        conn = sqlite3.connect("log.db")
+        c = conn.cursor()
+        timestamp = datetime.datetime.now()
+        c.execute("insert into employee (name, timestamp) values(?,?)", (name, timestamp))
+        conn.commit()
+        c.close()
+        conn.close()
     return(render_template("main.html", name=name))
 
 @app.route("/ethical_test", methods=['GET','POST'])
 def ethical_test():
     return(render_template("ethical_test.html"))
+
+@app.route("/query", methods=['GET','POST'])
+def query():
+    conn = sqlite3.connect("log.db")
+    c = conn.execute("select * from employee")
+    r = ""
+    for row in c:
+        r += str(row) + "<br>"
+    print(r)
+    r = Markup(r)
+    c.close()
+    conn.close()
+    return(render_template("query.html", r=r))
+
 
 @app.route("/answer", methods=['GET','POST'])
 def answer():
